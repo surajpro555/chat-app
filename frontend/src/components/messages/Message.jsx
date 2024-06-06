eb9651e99b3ae3094944e63ec50a5ac204fa7a1c
surajpro555 <surajprajapati1711@gmail.com>
2024-06-07 00:30:49 +0530

    socket

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -14,6 +14,7 @@ const Message = ({message}) => {
     ? authUser.profilePic
     : selectedConversation?.profilePic;
   const bubbleBgColor = fromMe ? "bg-blue-500" : "";
+	const shakeClass = message.shouldShake ? "shake" : "";
 
   return (
     <div className={`chat ${chatClassName}`}>
@@ -25,7 +26,7 @@ const Message = ({message}) => {
           />
         </div>
       </div>
-      <div className={`chat-bubble text-white  bg-blue-500 ${bubbleBgColor} pd-5`}>{message.message}</div>
+      <div className={`chat-bubble text-white  bg-blue-500 ${bubbleBgColor} ${shakeClass} pd-5`}>{message.message}</div>
       <div className="chat-footer opacity-50 text-xs flex gap-1 items-center">{formattedTime}</div>
     </div>
   );
